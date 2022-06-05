8e55809af117d5b8c09e84d6add9765c9a14795e
Hwa-J <hj1103.code@gmail.com>
2022-06-06 02:24:52 +0900

    feat: TodoItem.vue 컴포넌트 생성

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,182 @@
+<template>
+  <li>
+    <!-- done box -->
+    <div class="handle">
+      <i class="fas fa-grip-lines"></i>
+    </div>
+    <input
+      type="checkbox"
+      :checked="todo.done" 
+      @click="checkIt($event)" />
+    <!-- off edit -->
+    <template v-if="!editMode">
+      <span
+        class="title"
+        :class="{done: todo.done === true}">{{ todo.title }}</span>
+      <button
+        :disabled="todo.done"
+        @click.stop="onEditMode">
+        <i class="far fa-edit"></i>
+      </button>
+    </template>
+    <!-- on edit     -->
+    <template v-else>
+      <form
+        class="edit-form"
+        @submit.prevent
+        @click.stop>
+        <input
+          class="edit-input"
+          :value="title"
+          @change="editTitle($event)" />
+        <button
+          class="edit-btn"
+          type="submit"
+          @click="offEditMode">
+          <i class="fas fa-check"></i>
+        </button>
+        <button
+          class="cancle-btn"
+          type="button"
+          @click="offEditMode">
+          <i class="fas fa-ban"></i>
+        </button>
+      </form>
+    </template>
+    <!-- delete btn -->
+    <button
+      :data-id="todo.id"
+      @click="deleteTodo($event)">
+      <i class="far fa-minus-square"></i>
+    </button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true}
+  },
+  data() {
+    return {
+      editMode: false,
+    }
+  },
+  computed: {
+    todos() {
+      return this.$store.state.todos
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    onEditMode() {
+      this.editMode = true
+      this.title = this.todo.title
+      window.addEventListener('click', () => this.offEditMode())
+    },
+    offEditMode() {
+      this.editMode = false
+    },
+    editTitle(e) {
+      this.$emit('edit-title', e.target.value)
+    },
+    deleteTodo(e) {
+      const todoId = e.target.closest('button').getAttribute('data-id')
+      // e.target.dataset.id
+      this.$store.dispatch('deleteTodo', todoId)
+    },
+    checkIt(e) {
+      this.$emit('checked-done', e.target.checked)
+    },
+    doneTodo() {
+      if(this.done) {
+        return 'done'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  width: 100%;
+  height: 28px;
+  display: flex;
+  padding: 10px 0;
+  margin: 5px 0;
+  align-items: center;
+  border-radius: 4px;
+}
+li:hover {
+  background-color: $color-lightgray;
+  i {
+    opacity: .4;
+      &:hover{
+        opacity: 1;
+      }
+  }
+}
+li.sortable-ghost {
+  background-color: $color-gold;
+}
+li.sortable-drag {
+  opacity: 0 !important;
+}
+.handle {
+  color: gray;
+  margin-right: 5px;
+  &:hover {
+    cursor: grab;
+  }
+  &:active {
+    cursor: grabbing;
+  }
+}
+.title {
+  margin-left: 10px;
+  flex-grow: 1;
+  list-style: none;
+}
+.title.done {
+  color: gray;
+  text-decoration:line-through;
+}
+.edit-form {
+  position: relative;
+  @include center;
+  flex-wrap: wrap;
+  flex-grow: 1;
+  button {
+    i{
+      opacity: .4;
+    }
+    .fa-check {
+    color: green;
+  }
+    .fa-ban {
+      color: red;
+    }
+  }
+}
+input.edit-input {
+  margin-left: 10px;
+  flex-grow: 1;
+  font-size: 16px;
+  color: $color-darkgray;
+  @include input-focused;
+}
+i {
+  padding: 0 5px;
+  opacity: 0;
+}
+.fa-edit {
+  font: {
+    size: 15.2px
+  };
+}
+</style>
